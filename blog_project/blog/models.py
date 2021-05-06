1bdd0ecee3483cd2861c0d59fafeeef33422f635
10cheon00 <10cheon00@gmail.com>
2021-05-06 14:59:39 +0000

    Post model show on admin page

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,11 +1,10 @@
-import datetime
-
 from django.db import models
+from django.utils import timezone
 
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    create_at = models.DateTimeField(default=datetime.datetime)
+    create_at = models.DateTimeField(default=timezone.now)
 
     class Meta:
         ordering = ['-create_at']
