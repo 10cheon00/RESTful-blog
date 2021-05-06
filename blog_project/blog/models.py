002f2cb522b61b56505cd188f88cd60f423cb9f0
10cheon00 <10cheon00@gmail.com>
2021-05-06 14:47:03 +0000

    added model.

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,3 +1,14 @@
+import datetime
+
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    create_at = models.DateTimeField(default=datetime.datetime)
+
+    class Meta:
+        ordering = ['-create_at']
+
+    def __str__(self):
+        return self.title
