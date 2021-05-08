ec753f2d6acc59a351ba823c2909789325b02681
10cheon00 <10cheon00@gmail.com>
2021-05-08 03:03:54 +0000

    change listapiview to listcreateapiviwe

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,9 +1,9 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListCreateAPIView
 
 from blog.models import Post
 from blog.serializers import PostSerializer
 
 
-class PostListAPIView(ListAPIView):
+class PostListAPIView(ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
