864df346c548c329a685ab8fe1c7e373df126150
10cheon00 <10cheon00@gmail.com>
2021-05-08 02:11:40 +0000

    create sample apiview

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -3,8 +3,11 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, default='')
+
     create_at = models.DateTimeField(default=timezone.now)
+
+    content = models.TextField(max_length=100000, default='')
 
     class Meta:
         ordering = ['-create_at']
