128d6127c0b03d19c0d9be3ca582a25e5f79c988
10cheon00 <10cheon00@gmail.com>
2021-06-20 05:36:29 +0000

    uninstalled djangorestframework-jwt. change package it to djangorestframework-simplejwt.

--- blog_project/backend/profiles/urls.py
+++ blog_project/backend/profiles/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from rest_framework_jwt.views import obtain_jwt_token
-from rest_framework_jwt.views import verify_jwt_token
-from rest_framework_jwt.views import refresh_jwt_token
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenVerifyView
 
 
 app_name = 'profiles'
 
 urlpatterns = [
-    path('', obtain_jwt_token),
-    path('verify/', verify_jwt_token),
-    path('refresh/', refresh_jwt_token),
+    path('', TokenObtainPairView.as_view),
+    path('refresh/', TokenRefreshView.as_view),
+    path('verify/', TokenVerifyView.as_view),
 ]
