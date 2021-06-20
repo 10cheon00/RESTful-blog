425ab8105ba3197263e297d9e4134f1f9b45dbde
10cheon00 <10cheon00@gmail.com>
2021-06-20 14:36:40 +0000

    Created Signup view, serializer.

---
+++ blog_project/backend/profiles/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+
+from django.contrib.auth.models import User
+
+from backend.profiles.models import Profile
+
+
+class ProfileSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=20)
+
+    nickname = serializers.CharField(max_length=20)
+
+    password = serializers.CharField(write_only=True)
+
+    password_confirmation = serializers.CharField(write_only=True)
+
+    def save(self):
+        username = self.validated_data.get('username')
+
+        # TODO check already exists username.
+
+        user = User.objects.create_user(
+            username=username,
+            password=self.validated_data.get('password')
+        )
+
+        # TODO check already exists nickname.
+
+        profile = Profile(
+            user=user,
+            nickname=self.validated_data.get('nickname')
+        )
+        profile.save()
+
+        return profile
--- blog_project/backend/profiles/urls.py
+++ blog_project/backend/profiles/urls.py
@@ -3,11 +3,14 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.views import TokenRefreshView
 from rest_framework_simplejwt.views import TokenVerifyView
 
+from backend.profiles.views import SignUpAPIView
+
 
 app_name = 'profiles'
 
 urlpatterns = [
-    path('', TokenObtainPairView.as_view),
-    path('refresh/', TokenRefreshView.as_view),
-    path('verify/', TokenVerifyView.as_view),
+    path('', TokenObtainPairView.as_view()),
+    path('refresh/', TokenRefreshView.as_view()),
+    path('verify/', TokenVerifyView.as_view()),
+    path('signup/', SignUpAPIView.as_view()),
 ]
